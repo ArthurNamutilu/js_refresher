a05ecee6c69fa9cb137d2c5e709560dc9ebdcd07
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-27 21:24:10 +0300

    fresh

---
+++ userInput.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prompt the user to enter their name
+var userName = prompt("Please enter your name:");
+//userName = "Arthur";
+// Output the user's name
+console.log("Hello, " + userName + "!");
+
