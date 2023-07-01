1bae1c38157321bccfaede3e565c044b2db91004
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-01 21:44:32 +0300

    basic implementation of Node.js server

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { //server => instance
     if (req.url === '/') {
         res.write('Hello world');
         res.end();
