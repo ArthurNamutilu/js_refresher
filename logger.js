ab41b26d7da797cc810716dbd9f028c78353252f
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-01 10:49:48 +0300

    exporting & importing modules

--- logger.js
+++ logger.js
@@ -1,9 +1,9 @@
-let name = 'Gg';
 function log(message) {
     console.log(message);
 }
 
-module.exports = log;
-module.exports.userName = name;
+
+module.exports = log;        //exporting single functon
+//module.exports.log = name;  exporting as an object
 
 //console.log(module)
