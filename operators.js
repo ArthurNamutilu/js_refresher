fecd115d1e2183b76d8e3258c9415692fb9ce9e8
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-01 07:42:18 +0300

    operators.js

---
+++ operators.js
@@ -0,0 +1,13 @@
+// ternary operator
+const result  =true ? 'truthy' : 'Falsy'
+console.log(result)
+
+// guard operator &&
+const message = 1 && 'hello'
+console.log(message)
+
+// default operator &&
+// const currency = 'EUR' || 'USD'
+const currency = undefined || 'USD'
+
+console.log(currency)
