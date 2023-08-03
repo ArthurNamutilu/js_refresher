cebbca8bc8beadbddd6733bcd1c2098381c50a15
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-03 09:12:49 +0300

    dom javascript

---
+++ dom.js
@@ -0,0 +1,5 @@
+if (document.querySelector('.js-subscribe-button').innerHTML === 'Subscribe'){
+    document.querySelector('button').innerHTML = 'Subscribed'
+}else if (document.querySelector('button').innerHTML === 'Subscribed'){
+    document.querySelector('button').innerHTML = 'Subscribe'
+}
